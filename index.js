b7c056ceace6bdba1ef367bd765a59f89c6b189e
bidduthosen <mdbidduthosen34@gmail.com>
2022-10-25 15:20:25 +0600

    cors install and use

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 const courses = require('./data/courses.json')
 
@@ -13,6 +15,7 @@ app.get('/', (req, res)=>{
 app.get('/courses', (req, res)=>{
     res.send(courses);
 })
+
 app.listen( port, (req, res)=>{
     console.log(`digital learning platform server load ${port}`)
 })
